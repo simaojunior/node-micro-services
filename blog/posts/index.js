cd1da17e24c8dc6f740f5d85292ceb070ac467be
Simão Junior <simao.msjr@gmail.com>
2021-05-26 02:33:08 -0300

    feat: create routes posts

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,13 +1,24 @@
-const express = require('express');
+import express from 'express';
+import { randomBytes } from 'crypto';
 
 const app = express();
+app.use(express.json());
+
+const posts = {};
 
 app.get('/posts', (req, res) => {
-	req.send('Hello, world');
+	res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
+	const id = randomBytes(4).toString('hex');
+	const { title } = req.body;
 
+	posts[id] = {
+		id,
+		title
+	};
+	res.status(201).send(posts[id]);
 });
 
 app.listen(3333, () => {
